987500ef376b049a96c85ed0a6e7739789fe8527
wyx <wangyv_x@163.com>
2024-01-16 22:25:19 +0800

    finish lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,15 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node* hare=head;
+    node* tortoise=head;
+    while(1){
+        hare=hare->next;
+        if(hare==NULL){return 0;}
+        hare=hare->next;
+        if(hare==NULL){return 0;}
+        tortoise=tortoise->next;
+        if(tortoise==hare){return 1;}
+    }
     return 0;
 }
